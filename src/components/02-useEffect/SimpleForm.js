a668bc626e7af42eec5aa69087f072541e69a4d2
jssmm <jessenalexmercado@gmail.com>
2022-04-27 15:30:15 -0500

    useEffect para escuchar los cambios en el formuario

---
+++ src/components/02-useEffect/SimpleForm.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./effects.css";
+
+export const SimpleForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+  });
+
+  const { name, email } = formState;
+
+  useEffect(() => {
+    console.log("hey");
+  }, []);
+
+  useEffect(() => {
+    console.log("formState cambió");
+  }, [formState]);
+
+  const handleInputChange = ({ target }) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
+
+  return (
+    <>
+      <h1>useEffect</h1>
+      <hr />
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="name"
+          className="form-control"
+          autoComplete="off"
+          placeholder="Tu nombre"
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="text"
+          name="email"
+          className="form-control"
+          autoComplete="off"
+          placeholder="email@@email.com"
+          value={email}
+          onChange={handleInputChange}
+        />
+      </div>
+    </>
+  );
+};
